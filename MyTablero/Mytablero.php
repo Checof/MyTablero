31166a7d4eafffe1f82335fcdaa231b409e3cff7
Sergio Horta <programador@aaag.org.mx>
2024-11-01 13:09:07 -0600

    Se agrego el archivo myTablero.php

---
+++ MyTablero/Mytablero.php
@@ -0,0 +1,26 @@
+
+<!doctype html>
+<html lang="es-mx">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Sergio horta">
+    <meta name="generator" content="AAAG">
+    <link rel="icon" type="image/png" href="../img/favicon-aaag.png">
+    <title>AAAG - Circulares</title>
+
+    <link href="BS/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="BS/css/dashboard.css" rel="stylesheet">
+  </head>
+
+  <body>
+    
+    <div>"Hola mundo"</div>
+
+    <script src="BS/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
