1d31ab7ba1544e5846fe33c7bd8314433b551e1a
Sergio Horta <programador@aaag.org.mx>
2025-03-14 16:34:27 -0600

    Se añade archivo extension php con la estructura inicial del sitio

---
+++ MyTablero.php
@@ -0,0 +1,26 @@
+
+<!doctype html>
+<html lang="es-mx">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Sergio Horta">
+    <meta name="generator" content="AAAG">
+    <link rel="icon" type="image/png" href="../img/favicon-aaag.png">
+    <title>SAHM - Mi tablero</title>
+
+    <link href="BS/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="BS/css/dashboard.css" rel="stylesheet">
+  </head>
+
+  <body>
+    
+    <div>"Hola mundo!"</div>
+
+    <script src="BS/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
